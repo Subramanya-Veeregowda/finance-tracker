1d3f2904f7473c2830a33ac344352dbaffa967ef
Subramanya V <subramanyav2002@gmail.com>
2026-04-04 22:26:35 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 const BASE_URL = "https://finance-api-2qow.onrender.com/";
 
 export const getTransactions = async () => {
-  const res = await fetch(`${BASE_URL}/transactions`);
+  const res = await fetch(`${BASE_URL}transactions`);
   return res.json();
 };
 
 export const getSummary = async () => {
-  const res = await fetch(`${BASE_URL}/summary`);
+  const res = await fetch(`${BASE_URL}summary`);
   return res.json();
 };
